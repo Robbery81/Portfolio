2871e37841f972925144681eb353833a34cf1677
Aleksandr <alexandr.robbery@gmail.com>
2020-07-13 18:05:13 +0300

    MainSlider extends Slider

--- 3bringitup/src/js/main.js
+++ 3bringitup/src/js/main.js
@@ -1,8 +1,9 @@
-import Slider from './modules/slider';
+//import Slider from './modules/slider/slider';
+import MainSlider from './modules/slider/slider-main';
 import playVideo from './modules/playVideo';
 
 window.addEventListener('DOMContentLoaded', () => {
-    let slider = new Slider('.page', '.next');
+    let slider = new MainSlider({page: '.page', btns: '.next'});
     slider.render();
 
     let player = new playVideo('.showup .play', '.overlay');
