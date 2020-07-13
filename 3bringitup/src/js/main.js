0637cd7116a78b5f0116219a2c8bfd775e914332
Aleksandr <alexandr.robbery@gmail.com>
2020-07-13 17:41:01 +0300

    add slider

---
+++ 3bringitup/src/js/main.js
@@ -0,0 +1,6 @@
+import Slider from './modules/slider';
+
+window.addEventListener('DOMContentLoaded', () => {
+    let slider = new Slider('.page', '.next');
+    slider.render();
+});
---
+++ 3bringitup/src/js/modules/slider.js
@@ -0,0 +1,60 @@
+export default class Slider {
+    constructor(page, btns) {
+        this.page = document.querySelector(page);
+        this.slides = this.page.children;
+        this.btns = document.querySelectorAll(btns);
+        this.slideNumber = 1;
+    }
+
+    showSlide(n) {
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+
+        if (this.slideNumber == 0) {
+            this.slideNumber = this.slides.length;
+        }
+
+        if (this.slideNumber == this.slides.length + 1) {
+            this.slideNumber = 1;
+        }
+
+        this.slides[this.slideNumber - 1].style.display = 'block';
+    }
+
+    plusSlides(n) {
+        this.showSlide(this.slideNumber += n);
+    }
+
+    showFloatElement(slideIndex, elementSelector, delay) {
+
+        if (this.slideNumber == slideIndex) {
+            let item = document.querySelector(elementSelector);
+            item.style.display = 'none';
+
+            setTimeout(() => {
+                item.classList.remove("fadeOutDown");
+                item.classList.add('animated', "fadeInUp");
+                item.style.display = 'block';
+            }, delay);
+        }
+    }
+
+    render() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.plusSlides(1);
+                this.showFloatElement(2, '.hanson', 3000);
+            });
+
+            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideNumber = 1;
+                this.showSlide(this.slideNumber);
+            });
+        });
+        
+
+        this.showSlide(this.slideNumber);
+    }
+}
